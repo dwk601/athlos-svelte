3b8b0d5b1137eb375caa9fc161f93a9599ec5f67
Dongook <108056780+dwk601@users.noreply.github.com>
2024-10-24 11:31:42 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,21 @@
 	import '../app.css';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import { onMount } from 'svelte';
+	import { gsap } from 'gsap';
+	import { afterNavigate } from '$app/navigation';
+
+	function animate() {
+		gsap.from('main', { opacity: 0, scale: 0.95, duration: 0.3, ease: 'linear' });
+	}
+
+	onMount(() => {
+		animate();
+	});
+
+	afterNavigate(() => {
+		animate();
+	});
 </script>
 
 <div class="flex min-h-screen flex-col">
